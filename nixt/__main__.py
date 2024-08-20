266cb27a30fe0283807536d9a94511e9bcd68c58
Bart Thate <bthate@dds.nl>
2024-08-20 22:41:45 +0200

    add runtimes

---
+++ nixt/__main__.py
@@ -0,0 +1,83 @@
+# This file is placed in the Public Domain.
+# pylint: disable=C0413,W0212,W0613,W0718,E0401
+
+
+"main"
+
+
+import getpass
+import os
+import sys
+
+
+from .cmds    import Commands
+from .persist import skel
+from .errors  import errors, later
+from .main    import cmnd, enable, scan
+from .parse   import parse
+from .runtime import Cfg
+from .utils   import modnames
+
+
+from . import modules
+
+
+def srv(event):
+    "create service file (pipx)."
+    if event.args:
+        username = event.args[0]
+    else:
+        username  = getpass.getuser()
+    path = os.path.normpath(f"/home/{username}/.local/bin/")
+    txt = f"""[Unit]
+Description={Cfg.name.upper()}
+Requires=network-online.target
+After=network-online.target
+
+[Service]
+Type=simple
+User={username}
+Group={username}
+ExecStartPre={path}/{Cfg.name} skl
+ExecStart={path}/{Cfg.name}d
+ExitType=cgroup
+KillSignal=SIGKILL
+KillMode=control-group
+RemainAfterExit=yes
+Restart=no
+
+[Install]
+WantedBy=default.target"""
+    event.reply(txt)
+
+
+def wrapped():
+    "wrap main function."
+    wrap(main)
+
+
+def wrap(func):
+    "catch exceptions"
+    try:
+        func()
+    except (KeyboardInterrupt, EOFError):
+        print("")
+    except Exception as exc:
+        later(exc)
+    errors()
+
+
+def main():
+    "main"
+    Commands.add(srv)
+    parse(Cfg, " ".join(sys.argv[1:]))
+    skel()
+    enable(print)
+    Cfg.dis = Cfg.sets.dis
+    Cfg.mod = ",".join(modnames(modules))
+    scan(Cfg.mod, modules)
+    cmnd(Cfg.otxt, print)
+
+
+if __name__ == "__main__":
+    wrapped()
---
+++ nixt/daemon.py
@@ -0,0 +1,72 @@
+# This file is placed in the Public Domain.
+# pylint: disable=C0411,C0413,W0212,W0718,E0401
+
+
+"main"
+
+
+import os
+import sys
+
+
+from .persist import skel
+from .errors  import later
+from .main    import init, scan
+from .runtime import Cfg
+from .utils   import forever, pidfile, privileges
+
+
+from . import modules
+
+
+def daemon(verbose=False):
+    "switch to background."
+    pid = os.fork()
+    if pid != 0:
+        os._exit(0)
+    os.setsid()
+    pid2 = os.fork()
+    if pid2 != 0:
+        os._exit(0)
+    if not verbose:
+        with open('/dev/null', 'r', encoding="utf-8") as sis:
+            os.dup2(sis.fileno(), sys.stdin.fileno())
+        with open('/dev/null', 'a+', encoding="utf-8") as sos:
+            os.dup2(sos.fileno(), sys.stdout.fileno())
+        with open('/dev/null', 'a+', encoding="utf-8") as ses:
+            os.dup2(ses.fileno(), sys.stderr.fileno())
+    os.umask(0)
+    os.chdir("/")
+    os.nice(10)
+
+
+def wrap(func):
+    "catch exceptions"
+    try:
+        func()
+    except (KeyboardInterrupt, EOFError):
+        pass
+    except Exception as ex:
+        later(ex)
+
+
+def wrapped():
+    "wrap main function"
+    wrap(main)
+    os._exit(0)
+
+
+def main():
+    "main"
+    Cfg.mod += ",irc,rss"
+    daemon()
+    privileges(Cfg.user)
+    skel()
+    pidfile(Cfg.pidfile)
+    scan(Cfg.mod, modules)
+    init(Cfg.mod, modules)
+    forever()
+
+
+if __name__ == "__main__":
+    wrapped()
