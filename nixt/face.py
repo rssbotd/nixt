99599db93f7c91c0c6f34af8da97b78668ac546e
Bart Thate <rssbotd@gmail.com>
2024-09-14 15:38:59 +0200

    add face

---
+++ nixt/face.py
@@ -0,0 +1,18 @@
+# This file is placed in the Public Domain.
+
+
+"interface"
+
+
+from . import console, daemon, errors, main, object, reactor, service
+from . import thread, workdir
+
+
+from .console import *
+from .daemon  import *
+from .errors  import *
+from .main    import *
+from .object  import *
+from .reactor import *
+from .service import *
+from .thread  import *
